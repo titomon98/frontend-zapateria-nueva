b31c2b22aae2a41ec45deeaa26bd81c1fba74898
dabo1103 <batz.dev.404@gmail.com>
2024-06-26 23:40:10 -0600

    pepe

--- src/views/App/Services/Zapato.vue
+++ src/views/App/Services/Zapato.vue
@@ -617,6 +617,7 @@ export default {
           reader.onload = (event) => {
             this.form.base64Images.push(event.target.result)
           }
+
           reader.readAsDataURL(file)
         })
       }
